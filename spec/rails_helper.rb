b61a47ab2e8952087bb3be87c145cf6700591b09
Yousaf Nabi <yousafn@gmail.com>
2025-09-06 21:53:36 +0100

    chore(ci): no kafka gem on windows

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,7 +35,7 @@ require "rspec/rails"
 Dir["#{__dir__}/support/**/*.rb"].sort.each { |f| require f }
 
 # Optional dependencies
-unless RUBY_PLATFORM =~ /win32|x64-mingw32/
+unless RUBY_PLATFORM =~ /win32|x64-mingw32|x64-mingw-ucrt/
   require "sbmt/kafka_consumer"
   require "sbmt/kafka_producer"
 end
